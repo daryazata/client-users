b051016855df880d060c3ecc91ca894b01513b97
Darya Zata <darya.zata@outlook.com>
2022-04-04 20:13:35 +0200

    chore: add proces.env to server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require('./app.server');
 
-const port = 8080;
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
